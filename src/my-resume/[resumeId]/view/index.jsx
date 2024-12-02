65c3d08408ec96400b25ef197f86f58ac59562ee
unknown <tamiloreolaniyi@gmail.com>
2024-12-02 14:23:16 +0100

    share

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -39,7 +39,7 @@ function ViewResume() {
             <div className='flex justify-between px-44 my-10'>
                 <Button onClick={HandleDownload}>Download</Button>
                
-                <RWebShare
+        {/* <RWebShare
         data={{
           text: "Hello Everyone, This is my resume please open url to see it",
           url: import.meta.env.VITE_BASE_URL+"/my-resume/"+resumeId+"/view",
@@ -47,7 +47,7 @@ function ViewResume() {
         }}
         onClick={() => console.log("shared successfully!")}
       > <Button>Share</Button>
-      </RWebShare>
+      </RWebShare> */}
             </div>
         </div>
             
